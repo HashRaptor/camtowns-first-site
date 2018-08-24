ac87116ac2da35dd4c9257746bcf3c1e4784765e
Camron Weeks <Camron.Weeks@gmail.com>
2018-08-23 20:00:43 -0400

    removing unneeded code

--- src/Home.js
+++ src/Home.js
@@ -1,58 +1,31 @@
 import React, { Component } from "react";
-import { Grid, Row, Col } from 'react-bootstrap';
-const dummySentences = [
-  'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
-  'Donec hendrerit tempor tellus.',
-  'Donec pretium posuere tellus.',
-  'Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus.',
-  'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
-  'Nulla posuere.',
-  'Donec vitae dolor.',
-  'Nullam tristique diam non turpis.',
-  'Cras placerat accumsan nulla.',
-  'Nullam rutrum.',
-  'Nam vestibulum accumsan nisl.'
-];
+import { Button, OverlayTrigger, Tooltip } from 'react-bootstrap';
+
+
+function LinkWithTooltip({ id, children, href, tooltip }) {
+  return (
+    <OverlayTrigger
+      overlay={<Tooltip id={id}>{tooltip}</Tooltip>}
+      placement="top"
+      delayShow={300}
+      delayHide={150}
+    >
+      <a href={href}>{children}</a>
+    </OverlayTrigger>
+  );
+}
+
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <Grid>
-          <Row className="show-grid">
-            <Col sm={6} md={3}>
-              <code>&lt;{'Col sm={6} md={3}'} /&gt;</code>
-              <br />
-              {dummySentences.slice(0, 6).join(' ')}
-            </Col>
-            <Col sm={6} md={3}>
-              <code>&lt;{'Col sm={6} md={3}'} /&gt;</code>
-              <br />
-              {dummySentences.slice(0, 4).join(' ')}
-            </Col>
-            <Col sm={6} md={3}>
-              <code>&lt;{'Col sm={6} md={3}'} /&gt;</code>
-              <br />
-              {dummySentences.slice(0, 6).join(' ')}
-            </Col>
-            <Col sm={6} md={3}>
-              <code>&lt;{'Col sm={6} md={3}'} /&gt;</code>
-              <br />
-              {dummySentences.slice(0, 2).join(' ')}
-            </Col>
-          </Row>
-        </Grid>
+        <p>I'm a{' '}
+          <LinkWithTooltip tooltip="crazy person" href="#" id="tooltip-1">
+            software engineer
+          </LinkWithTooltip>.
+        </p>
 
-
-        <h2>HELLO</h2>
-        <p>Cras facilisis urna ornare ex volutpat, et
-        convallis erat elementum. Ut aliquam, ipsum vitae
-        gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-        metus nec massa. Maecenas hendrerit laoreet augue
-        nec molestie. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus.</p>
-
-        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
       </div>
     );
   }
--- src/Main.js
+++ src/Main.js
@@ -13,7 +13,6 @@ class Main extends Component {
     return (
       <HashRouter>
         <div>
-          <h1>Welcome to Camtown's SPA</h1>
           <ul className="header">
             <li><NavLink exact to="/">Home</NavLink></li>
             <li><NavLink to="/resume">Resume</NavLink></li>
